807e63aeb479fe174173a6fffc210033bceb5cf6
statefb <tmgf.15.su@gmail.com>
2025-09-10 21:08:35 +0900

    fix: ambiguity worker as docker prisma function

--- cdk/lib/constructs/ambiguity-detection-processor.ts
+++ cdk/lib/constructs/ambiguity-detection-processor.ts
@@ -2,13 +2,14 @@ import { Construct } from "constructs";
 import * as cdk from "aws-cdk-lib";
 import * as sqs from "aws-cdk-lib/aws-sqs";
 import * as lambda from "aws-cdk-lib/aws-lambda";
-import * as lambdaNodejs from "aws-cdk-lib/aws-lambda-nodejs";
 import * as lambdaEventSources from "aws-cdk-lib/aws-lambda-event-sources";
 import * as ec2 from "aws-cdk-lib/aws-ec2";
 import * as iam from "aws-cdk-lib/aws-iam";
 import * as path from "path";
 import { Duration } from "aws-cdk-lib";
+import { Platform } from "aws-cdk-lib/aws-ecr-assets";
 import { DatabaseConnectionProps } from "./database";
+import { DockerPrismaFunction } from "./docker-prisma-function";
 
 export interface AmbiguityDetectionProcessorProps {
   vpc: ec2.Vpc;
@@ -19,22 +20,22 @@ export interface AmbiguityDetectionProcessorProps {
 export class AmbiguityDetectionProcessor extends Construct {
   public readonly queue: sqs.Queue;
   public readonly dlq: sqs.Queue;
-  public readonly workerLambda: lambdaNodejs.NodejsFunction;
+  public readonly workerLambda: lambda.Function;
   public readonly securityGroup: ec2.SecurityGroup;
 
-  constructor(scope: Construct, id: string, props: AmbiguityDetectionProcessorProps) {
+  constructor(
+    scope: Construct,
+    id: string,
+    props: AmbiguityDetectionProcessorProps
+  ) {
     super(scope, id);
 
     // Security group for Worker Lambda
-    this.securityGroup = new ec2.SecurityGroup(
-      this,
-      "WorkerSecurityGroup",
-      {
-        vpc: props.vpc,
-        description: "Security group for Ambiguity Detection Worker Lambda",
-        allowAllOutbound: true,
-      }
-    );
+    this.securityGroup = new ec2.SecurityGroup(this, "WorkerSecurityGroup", {
+      vpc: props.vpc,
+      description: "Security group for Ambiguity Detection Worker Lambda",
+      allowAllOutbound: true,
+    });
 
     // Dead letter queue for failed messages
     this.dlq = new sqs.Queue(this, "DLQ", {
@@ -45,36 +46,36 @@ export class AmbiguityDetectionProcessor extends Construct {
     // Main queue for ambiguity detection tasks
     this.queue = new sqs.Queue(this, "Queue", {
       queueName: "ambiguity-detection-queue",
-      visibilityTimeout: Duration.minutes(15),
+      visibilityTimeout: Duration.minutes(17), // Lambda timeout (15min) + 2min buffer
       enforceSSL: true,
       deadLetterQueue: {
         queue: this.dlq,
-        maxReceiveCount: 3,
+        maxReceiveCount: 2,
       },
     });
 
     // Worker Lambda function
-    this.workerLambda = new lambdaNodejs.NodejsFunction(
-      this,
-      "WorkerLambda",
-      {
-        entry: path.join(__dirname, "../../../backend/src/handlers/ambiguity-detection-worker.ts"),
-        handler: "handler",
-        runtime: lambda.Runtime.NODEJS_22_X,
-        timeout: Duration.minutes(15),
-        memorySize: 1024,
-        environment: {
-          DATABASE_SECRET_ARN: props.databaseConnection.secret.secretArn,
-          DATABASE_OPTION: "?pool_timeout=20&connect_timeout=20",
-          BEDROCK_REGION: props.bedrockRegion,
-        },
-        vpc: props.vpc,
-        vpcSubnets: {
-          subnetType: ec2.SubnetType.PRIVATE_WITH_EGRESS,
-        },
-        securityGroups: [this.securityGroup],
-      }
-    );
+    this.workerLambda = new DockerPrismaFunction(this, "WorkerLambda", {
+      code: lambda.DockerImageCode.fromImageAsset(
+        path.join(__dirname, "../../../backend/"),
+        {
+          file: "Dockerfile.prisma.lambda",
+          platform: Platform.LINUX_ARM64,
+          cmd: ["dist/handlers/ambiguity-detection-handler.handler"],
+        }
+      ),
+      memorySize: 1024,
+      timeout: Duration.minutes(15),
+      vpc: props.vpc,
+      vpcSubnets: {
+        subnetType: ec2.SubnetType.PRIVATE_WITH_EGRESS,
+      },
+      securityGroups: [this.securityGroup],
+      database: props.databaseConnection,
+      environment: {
+        BEDROCK_REGION: props.bedrockRegion,
+      },
+    });
 
     // Connect Lambda function to SQS event source
     this.workerLambda.addEventSource(
